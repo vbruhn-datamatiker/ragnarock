66437ce05c65951f691bbcc50ba96b04070ec7a9
Victoria Bruhn <vic.bruhn@hotmail.com>
2026-03-16 12:28:55 +0100

    12.22PM Forsøger stadig at få tab til at virke

--- ragnarock/wwwroot/js/site.js
+++ ragnarock/wwwroot/js/site.js
@@ -3,25 +3,18 @@
 
 // Write your JavaScript code.
 
+document.addEventListener("DOMContentLoaded", function () {
+    const tabs = document.querySelectorAll(".tablinks");
 
-function openPage(evt, pageName) {
-    //Declare variables
-    var i, tabcontent, tablinks;
+    tabs.forEach(tab => {
+        tab.addEventListener("click", function () {
 
-    //Get all elements with class="tabcontent" and hide them
+            document.querySelectorAll(".tabcontent").forEach(content => {
+                content.style.display = "none";
+            });
 
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    //Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(pageName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+            const tabId = this.getAttribute("tab");
+            document.getElementById(tabId).style.display = "block";
+        });
+    });
+});
