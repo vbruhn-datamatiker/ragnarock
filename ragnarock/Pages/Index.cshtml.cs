2af6b8ba2b67a3c6fb1aa364ac481c161d9162f7
Victoria Bruhn <vic.bruhn@hotmail.com>
2026-03-16 09:50:49 +0100

    Add project files.

---
+++ ragnarock/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ragnarock.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
